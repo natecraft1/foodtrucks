1daba6dc17c4247135048e01ae5e97939a382cb7
Baxter Ross <baxterross@gmail.com>
2013-08-05 12:42:19 -0700

    y

---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,65 @@
+class TwitterController < ApplicationController
+
+  def search
+    # open-uri is necessary to be able to use the CGI::escape method
+    require 'open-uri'
+    
+    # get our query from the url
+    @query = CGI::escape params[:q]
+    
+    # the base url of the twitter search api
+    baseurl = 'https://api.twitter.com/1.1/search/tweets.json'
+    
+    # build a request url using the user-specified query
+    url = baseurl + '?q=' + @query
+
+    # initialize an empty array to hold the tweets
+    @tweets = []
+    
+    # use the getTwitterData method to perform the request and return the response
+    data = getTwitterData(url)
+    
+    # push each response into the @tweets array
+    data['statuses'].each do |status|
+      @tweets.push status
+    end
+    
+    # get the url of the next page of results
+    next_page_url = data['search_metadata']['next_results']
+    
+    # build a url which we could use to get another page of data
+    url = baseurl + next_page_url
+
+    # uncomment the next line to dump out @tweets as json for debugging
+    # render json: @tweets
+  end
+
+  def getTwitterData (url)
+    # define our app's public and private keys
+    consumer_key = '0dwuDZkLEtJNotiBa5GHgg'
+    consumer_secret = 'gML2Gr91KAHnnUi7c7Vi7ZWINx2FUIAOdUt6TSN8'
+    
+    # define our user access tokens
+    access_token = '19741986-XevrLQiKjIPnsk3nVfazWHtZvaj1tfZZTvum2kmKx'
+    access_token_secret = 'bnbgNUhyqtMtdL0yuxG27c6CfSarSbmdJQLSIIAdI'
+    
+    # instantiate a Consumer object using our app's keys
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {
+      :site => 'https://api.twitter.com',
+      :scheme => :header
+    })
+    
+    # define an AccessToken object using a combination of our Consumer object and out access tokens
+    request_wrapper = OAuth::AccessToken.from_hash(consumer,
+      oauth_token: access_token,
+      oauth_token_secret: access_token_secret
+    )
+    
+    # send a GET request to the URL
+    response = request_wrapper.request(:get, url)
+    
+    # parse the response as JSON and return it
+    JSON.parse(response.body)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OauthExample::Application.config.secret_key_base = 'ba6591405044369412abb7d6eaa5a2ec02ba77b67c243d87bf575ffbe26d5be846e3307d40e1ac373c918d6bfc3ebadce4256cccb0d9b1d63a39a1b9a93b621c'
